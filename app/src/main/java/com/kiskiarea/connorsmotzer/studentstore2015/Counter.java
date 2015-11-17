e270abf116f06a684524bcc30d49b6cc93ee5f59
CONNOR SMOTZER <CONNOR.SMOTZER@kiskiarea.com>
2015-11-17 08:10:28 -0500

    Initial commit

---
+++ app/src/main/java/com/kiskiarea/connorsmotzer/studentstore2015/Counter.java
@@ -0,0 +1,17 @@
+package com.kiskiarea.connorsmotzer.studentstore2015;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Connor on 11/11/2015.
+ */
+public class Counter extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.counter_layout);
+    }
+}
---
+++ app/src/main/java/com/kiskiarea/connorsmotzer/studentstore2015/Deals.java
@@ -0,0 +1,17 @@
+package com.kiskiarea.connorsmotzer.studentstore2015;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Connor on 11/11/2015.
+ */
+public class Deals extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deals_layout);
+    }
+}
